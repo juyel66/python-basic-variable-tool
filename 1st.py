7022708c89ae82a9165d49f5adc789423dfe1618
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-10-05 02:26:58 +0600

    Add initial implementation of circle area calculation and basic math operations

---
+++ 1st.py
@@ -0,0 +1,51 @@
+from math import *
+
+# a = 2
+# b=3
+# c = a+b
+# print("The sum of a  and b is:",c)
+
+# print(15+5)
+# print(15-5)
+# print(15*5)
+# print(15/5)
+# print(14854%5)
+
+
+
+# num1= int (input("Enter your first number:"))
+# num2 = int (input("Enter your second number:"))
+# sum =  num1 + num2
+# sub =  num1 - num2
+# mul =  num1 * num2
+# dev =  num1 / num2
+# mod =  num1 % num2
+# print("The sum of two numbers is:", sum)
+# print("The sum of two numbers is:", sub)
+# print("The sum of two numbers is:", mul)
+# print("The sum of two numbers is:", dev)
+# print("The sum of two numbers is:", mod)
+
+
+# # triangle area
+# base = int (input("Enter the base of triangle:"))
+# height = int (input("Enter the height of triangle:"))
+
+# area = 1/2 * base * height
+# print("The area of triangle is:", area)
+
+
+# circle area 
+
+r = int (input("Enter the radius of circle:"))
+pi = 3.1416
+area = pi * r * r
+print("The area of circle is:", area)
+
+
+
+print(max(5,10))
+print(min(5,10))
+print(abs(-5))
+print(pow(5,10))
+print(sqrt(5))
